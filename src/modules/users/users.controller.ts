d29974263559fd6308004bc2f9296f708eb2d8cc
renan5g <renansilva.dev5g@gmail.com>
2021-12-29 08:59:13 -0300

    feat: kafka connect client

---
+++ src/modules/users/users.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, OnModuleInit } from '@nestjs/common';
+import { Client, ClientKafka, Transport } from '@nestjs/microservices';
+
+@Controller('users')
+export class UsersController implements OnModuleInit {
+  @Client({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'user',
+        brokers: ['localhost:9092'],
+      },
+      consumer: {
+        groupId: 'user-consumer',
+        allowAutoTopicCreation: true,
+      },
+    },
+  })
+  private client: ClientKafka;
+
+  async onModuleInit() {
+    const requestPatters = ['find-all-user', 'create-user'];
+
+    requestPatters.forEach(async (pattern) => {
+      this.client.subscribeToResponseOf(pattern);
+      await this.client.connect();
+    });
+  }
+}
